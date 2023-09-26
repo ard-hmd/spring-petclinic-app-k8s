6fcf88ef65008a8d03dc18991b20662338c0f48b
michelnguyenfr <michel.nguyen.fr@gmail.com>
2023-09-26 17:17:59 +0200

    fix hem install

--- spring-petclinic-chart/launcher.sh
+++ spring-petclinic-chart/launcher.sh
@@ -16,13 +16,45 @@ kubectl create secret generic customers-db-mysql --from-literal=mysql-root-passw
 kubectl create secret generic vets-db-mysql --from-literal=mysql-root-password=password --namespace $ENVIRONNMENT && \
 kubectl create secret generic visits-db-mysql --from-literal=mysql-root-password=password --namespace $ENVIRONNMENT
 
-helm install $ENVIRONNMENT . \
+helm install spring-resources chart-resources \
 	--set namespace=$ENVIRONNMENT \
-	--set repository_prefix=ardhmd \
+	--set repository_prefix=michelnguyenfr \
 	--set dbhost_customers=$DBHOST \
 	--set dbhost_vets=$DBHOST \
-	--set dbhost_visits=$DBHOST
+	--set dbhost_visits=$DBHOST \
+	-n $ENVIRONNMENT
+
+helm install spring-api-gateway chart-api-gateway \
+	--set namespace=$ENVIRONNMENT \
+	--set repository_prefix=michelnguyenfr \
+	--set dbhost_customers=$DBHOST \
+	--set dbhost_vets=$DBHOST \
+	--set dbhost_visits=$DBHOST \
+	-n $ENVIRONNMENT
+
+helm install spring-customers chart-customers \
+	--set namespace=$ENVIRONNMENT \
+	--set repository_prefix=michelnguyenfr \
+	--set dbhost_customers=$DBHOST \
+	--set dbhost_vets=$DBHOST \
+	--set dbhost_visits=$DBHOST \
+	-n $ENVIRONNMENT
 	
+helm install spring-vets chart-vets \
+	--set namespace=$ENVIRONNMENT \
+	--set repository_prefix=michelnguyenfr \
+	--set dbhost_customers=$DBHOST \
+	--set dbhost_vets=$DBHOST \
+	--set dbhost_visits=$DBHOST \
+	-n $ENVIRONNMENT
+
+helm install spring-visits chart-visits \
+	--set namespace=$ENVIRONNMENT \
+	--set repository_prefix=michelnguyenfr \
+	--set dbhost_customers=$DBHOST \
+	--set dbhost_vets=$DBHOST \
+	--set dbhost_visits=$DBHOST \
+	-n $ENVIRONNMENT
 
 echo
 echo "You can track the progress with the following command:"
