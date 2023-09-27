119ad5a18096f729780f69d98a78d2ff3c800b1c
michelnguyenfr <michel.nguyen.fr@gmail.com>
2023-09-27 14:55:11 +0200

    factorizing the helm calls

--- spring-petclinic-chart/launcher.sh
+++ spring-petclinic-chart/launcher.sh
@@ -16,78 +16,25 @@ kubectl create secret generic customers-db-mysql --from-literal=mysql-root-passw
 kubectl create secret generic vets-db-mysql --from-literal=mysql-root-password=password --namespace $ENVIRONNMENT && \
 kubectl create secret generic visits-db-mysql --from-literal=mysql-root-password=password --namespace $ENVIRONNMENT
 
-helm upgrade --install spring-resources chart-resources \
-	--set namespace=$ENVIRONNMENT \
-	--set repository_prefix=michelnguyenfr \
-	--set dbhost_customers=$DBHOST \
-	--set dbhost_vets=$DBHOST \
-	--set dbhost_visits=$DBHOST \
-	--set dbname_visits=visitsdb \
-	--set dbuser_visits=admin \
-	--set dbname_customers=customersdb \
-	--set dbuser_customers=admin \
-	--set dbname_vets=vetsdb \
-	--set dbuser_vets=admin \
-	-n $ENVIRONNMENT
+service_names = ('resources' 'api-gateway' 'customers' 'vets' 'visits')
 
-echo "pause for 10sec for the resources to be online before deploying the microservices"
-sleep 10 
+for service_name in ${service_names[@]}
+do
+	helm upgrade --install spring-$service_name chart-$service_name \
+		--set namespace=$ENVIRONNMENT \
+		--set repository_prefix=michelnguyenfr \
+		--set dbhost_customers=$DBHOST \
+		--set dbhost_vets=$DBHOST \
+		--set dbhost_visits=$DBHOST \
+		--set dbname_visits=visitsdb \
+		--set dbuser_visits=admin \
+		--set dbname_customers=customersdb \
+		--set dbuser_customers=admin \
+		--set dbname_vets=vetsdb \
+		--set dbuser_vets=admin \
+		-n $ENVIRONNMENT
+done
 
-helm upgrade --install spring-api-gateway chart-api-gateway \
-	--set namespace=$ENVIRONNMENT \
-	--set repository_prefix=michelnguyenfr \
-	--set dbhost_customers=$DBHOST \
-	--set dbhost_vets=$DBHOST \
-	--set dbhost_visits=$DBHOST \
-	--set dbname_visits=visitsdb \
-	--set dbuser_visits=admin \
-	--set dbname_customers=customersdb \
-	--set dbuser_customers=admin \
-	--set dbname_vets=vetsdb \
-	--set dbuser_vets=admin \
-	-n $ENVIRONNMENT
-
-helm upgrade --install spring-customers chart-customers \
-	--set namespace=$ENVIRONNMENT \
-	--set repository_prefix=michelnguyenfr \
-	--set dbhost_customers=$DBHOST \
-	--set dbhost_vets=$DBHOST \
-	--set dbhost_visits=$DBHOST \
-	--set dbname_visits=visitsdb \
-	--set dbuser_visits=admin \
-	--set dbname_customers=customersdb \
-	--set dbuser_customers=admin \
-	--set dbname_vets=vetsdb \
-	--set dbuser_vets=admin \
-	-n $ENVIRONNMENT
-	
-helm upgrade --install spring-vets chart-vets \
-	--set namespace=$ENVIRONNMENT \
-	--set repository_prefix=michelnguyenfr \
-	--set dbhost_customers=customersdb.$DBHOST \
-	--set dbhost_vets=vetsdb.$DBHOST \
-	--set dbhost_visits=visitsdb.$DBHOST \
-	--set dbname_visits=visitsdb \
-	--set dbuser_visits=admin \
-	--set dbname_customers=customersdb \
-	--set dbuser_customers=admin \
-	--set dbname_vets=vetsdb \
-	--set dbuser_vets=admin \
-	-n $ENVIRONNMENT
-
-helm upgrade --install spring-visits chart-visits \
-	--set namespace=$ENVIRONNMENT \
-	--set repository_prefix=michelnguyenfr \
-	--set dbhost_customers=$DBHOST \
-	--set dbhost_vets=$DBHOST \
-	--set dbhost_visits=$DBHOST \
-	--set dbname_visits=visitsdb \
-	--set dbuser_visits=admin \
-	--set dbname_customers=customersdb \
-	--set dbuser_customers=admin \
-	--set dbname_vets=vetsdb \
-	--set dbuser_vets=admin \
-	-n $ENVIRONNMENT
 
 echo
 echo "You can track the progress with the following command:"
